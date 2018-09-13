aaa5e58fc6d9097ca74ecb852886b18b41ae0664
Harlan Heath <harlandheath@gmail.com>
2018-09-12 21:42:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Mah Clone! :O</p>
+      </div>
+    );
+  }
+}
+
+export default App;
